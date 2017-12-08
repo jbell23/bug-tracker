0cd752eb49f25c6dddc82d85a903da44fafc4ca2
Joseph <j.bell6666@student.leedsbeckett.ac.uk>
2017-12-08 17:56:08 +0000

    Created a database to run alongside the program. This database will store information about bugs and who reported them.

--- BugTracker/BugTracker/Form1.cs
+++ BugTracker/BugTracker/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,15 @@ namespace BugTracker
 {
     public partial class Form1 : Form
     {
+        SqlConnection mySqlConnection;
+
         public Form1()
         {
             InitializeComponent();
+
+            mySqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Joe\Documents\bugDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
